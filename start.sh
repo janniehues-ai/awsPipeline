d4b6bb4be3e0d73042b05388dbd837c5effada8e
root <root@ip-10-0-1-53.eu-north-1.compute.internal>
2025-11-04 15:07:28 +0000

    Moved worker registering to worker

--- start.sh
+++ start.sh
@@ -6,13 +6,7 @@ docker compose pull
 
 cp env.example .env
 touch dex.env
+echo $pipelineBasicauth >> traefik/auth/basicauth.txt
 
 docker compose up -d
-
-sleep 10
-docker compose exec redis redis-cli sadd groups:presenter admin@example.com
-
-#docker compose exec streamingasr bash -c "curl -H 'Content-Type: application/json' http://ltapi:5000/ltapi/register_worker -d '{\"component\": \"asr\", \"name\": \"mult57\", \"server\": \"http://asr.${DOMAIN}/asr\"}'"
-#docker compose exec streamingasr bash -c "curl -H 'Content-Type: application/json' http://ltapi:5000/ltapi/register_worker -d '{\"component\": \"mt\", \"name\": \"mult16\", \"server\": \"http://mt.${DOMAIN}\"}'"
-#docker compose exec streamingasr bash -c "curl -H 'Content-Type: application/json' http://ltapi:5000/ltapi/register_worker -d '{\"component\": \"tts\", \"name\": \"mult\", \"server\": \"http://tts.${DOMAIN}/tts\"}'"
 
