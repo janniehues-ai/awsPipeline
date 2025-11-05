1b0fce45889048584c100621b4e7683a2407ea76
root <root@ip-10-0-1-63.eu-north-1.compute.internal>
2025-11-05 14:29:30 +0000

    Renaming

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ docker compose pull
 
 cp env.example .env
 touch dex.env
-echo $pipelineBasicauth >> traefik/auth/basicauth.txt
+echo $BASICAUTHHASH >> traefik/auth/basicauth.txt
 
 docker compose up -d
 
